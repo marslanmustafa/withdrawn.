eba988c8c0229b002d90e5127cd2838c7a74aaf6
Muhammad Arslan <marslanmustafa391@gmail.com>
2024-01-26 12:36:12 +0500

    changed to ts format

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css'
-import Login from './pages/login/Login'
+import "./App.css";
+import Login from "./pages/login/Login";
 import SearchPage from "./pages/searchPage/SearchPage";
 
 function App() {
-
   return (
-    <div id='app'>
-    <Router>
+    <div id="app">
+      <Router>
         <Routes>
           <Route path="/search" element={<SearchPage />} />
           <Route path="/" element={<Login />} />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
